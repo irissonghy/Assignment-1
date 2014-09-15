b2023bbfc9cccc33bba80b13aa293a52baf7c8d6
irissonghy <huaying@umich.edu>
2014-09-14 22:29:24 -0400

    implemented complex calculator class
    this is my first java assignment

---
+++ SimpleCalculator.java
@@ -0,0 +1,26 @@
+//Huaying Song
+package calc;
+
+public class SimpleCalculator {
+
+	private double currentNumber;
+	
+	public SimpleCalculator(double d) {
+		
+		this.currentNumber = d;
+		
+	}
+	
+	public void add(double d) {
+		
+		currentNumber = currentNumber + d;
+		
+	}
+	
+	public double getCurrentNumber() {
+		
+		return currentNumber;
+		
+	}
+	
+}
